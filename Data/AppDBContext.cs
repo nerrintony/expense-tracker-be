a76575efb14b4d5e061610af40d7596d2e26e23c
nerrin <nirrentony@gmail.com>
2026-02-04 17:38:04 +0530

    Database connection

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTrackerBE.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ExpenseTrackerBE.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+    }
+}
